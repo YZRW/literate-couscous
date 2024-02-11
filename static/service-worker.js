492eb80bb08c74b8ef59831e417e2f8126e7ac14
moon <RWTighnari@icloud.com>
2024-02-11 20:34:01 +0800

    修改：     static/service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -20,7 +20,7 @@ if (workbox) {
 
   workbox.routing.registerRoute(
     // Cache image files
-    /.*\.(?:png|jpg|jpeg|svg|gif)/,
+    /\.(?:png|jpg|jpeg|svg|gif)$/,
     // Use the cache if it's available
     workbox.strategies.cacheFirst({
       // Use a custom cache name
